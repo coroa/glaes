212db6ab49c364ab03d4821d4d9b07eb69d2b885
Jonas Hoersch <jonas.hoersch@climateanalytics.org>
2023-09-05 18:18:39 +0200

    Bump version to v1.3.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="glaes",
-    version="1.3.1",
+    version="1.3.2",
     author="Severin Ryberg",
     url="https://github.com/coroa/glaes",
     packages=find_packages(),
